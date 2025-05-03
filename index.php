5191f4c90f92b5932c474e451522f2d5a582f24a
VictorRMarcelino <httpsvictorramos@gmail.com>
2025-05-03 20:02:30 +0000

    Exception Handler

--- index.php
+++ index.php
@@ -3,5 +3,6 @@
 use routes\Router;
 
 require_once 'vendor/autoload.php';
+require_once 'src/exception/ApiException.php';
 
 Router::getInstance()->doRequest();
---
+++ src/exception/ApiException.php
@@ -0,0 +1,9 @@
+<?php
+
+set_exception_handler(function(Exception $oException) {
+    http_response_code($oException->getCode());
+    $aResponse = [];
+    $aResponse['code'] = $oException->getCode();
+    $aResponse['message'] = $oException->getMessage();
+    echo json_encode($aResponse);
+});
